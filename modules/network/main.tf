7effa9e47de000df8a60e8a92eef7aed697cdd8c
noh4nsen <noh4nsen@gmail.com>
2025-06-16 20:17:20 -0300

    feat: enhance subnet configuration with delegations and service endpoints

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -7,22 +7,18 @@ resource "azurerm_virtual_network" "this" {
 
 resource "azurerm_subnet" "this" {
   for_each             = local.subnet_cfg
-
-  name                 = each.key
+  name                 = each.value.name
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = each.value.address_prefixes
 
-  service_endpoints    = try(each.value.service_endpoints, null)
+  service_endpoints    = each.value.service_endpoints
 
   dynamic "delegation" {
-    for_each = each.value.delegations != null ? each.value.delegations : []
-
+    for_each = each.value.delegations
     content {
       name = delegation.value.name
-      service_delegation {
-        name = delegation.value.service
-      }
+      service_delegation { name = delegation.value.service }
     }
   }
 }
